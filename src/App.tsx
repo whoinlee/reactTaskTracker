56ca3ce88c68c8a825fe27bd40dcb8948e701b50
Whoin Lee <whoinlee@gmail.com>
2021-08-03 10:49:50 -0400

    dynamic data fetching w. json-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 //-- Components
 import Header from './components/Header';
@@ -12,43 +12,108 @@ import { Wrapper } from './App.styles';
 import { TaskType } from './components/DataTypes';
 
 
-const tasksArr:TaskType[] = [
-  {
-      "text": "Doctors Appointment",
-      "day": "Feb 6th at 1:30pm",
-      "reminder": true,
-      "id": 1
-    },
-  {
-      "text": "Meeting at School",
-      "day": "Feb 6th at 1:30pm",
-      "reminder": true,
-      "id": 2
-  },
-  {
-      "text": "new task",
-      "day": "evening",
-      "reminder": false,
-      "id": 3
-  },
-]
-function App() {
-  const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState(tasksArr);
+//-- moved to db.son
+// const tasksArr:TaskType[] = [
+//   {
+//       "text": "Doctors Appointment",
+//       "day": "Feb 6th at 1:30pm",
+//       "reminder": true,
+//       "id": 1
+//     },
+//   {
+//       "text": "Meeting at School",
+//       "day": "Feb 6th at 1:30pm",
+//       "reminder": true,
+//       "id": 2
+//   },
+//   {
+//       "text": "new task",
+//       "day": "evening",
+//       "reminder": false,
+//       "id": 3
+// }]
+const tasksArr:TaskType[] = [];
+const API_URL = 'http://localhost:5000/tasks'; //-- through json-server
 
-  const addTask = (task:TaskType) => {
-    setTasks([...tasks, task]);
-    // console.log("App:: task is ", task);
+function App() {
+  const [tasks, setTasks] = useState(tasksArr);
+  const [showAddTask, setShowAddTask] = useState(false);
+  useEffect(() => {       //useEffect calling function can't be an async, useEffect(async() => ) (X)
+    // const fetchTasks = async() => {
+    //   const data = await(await fetch(API_URL)).json();
+    // }
+    // fetchTasks();
+    const getTasks = async() => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks();
+  }, []);
+
+  const fetchTasks = async() => {
+    const res = await fetch(API_URL);  
+    const data = await res.json();
+    //console.log("App:: fetchTasks, data is ", data); ===> return data
+    return data;
   };
-  const deleteTask = (id:number) => {
+
+  const fetchATask = async(id:number) => {
+    const res = await fetch(`${API_URL}/${id}`);  
+    const data = await res.json();
+    //console.log("App:: fetchATask, data is ", data);
+    return data;
+  }
+
+  // const addTask = (task:TaskType) => {
+  //   setTasks([...tasks, task]);
+  // };
+  const addTask = async (task:TaskType) => {
+    // console.log("App:: addTask, task is ", task);
+    // const res = await fetch(API_URL, {
+    await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    });
+    //-- the following code had some type issue
+    // const data = await res.json();
+    // setTasks([...tasks, data]);
+    setTasks([...tasks, task]);
+  };
+
+  // const deleteTask = (id:number) => {
+  //   setTasks(tasks.filter((task) => task.id !== id));
+  // };
+  const deleteTask = async (id:number) => {
     // console.log('App :: deleteTask');
+    await fetch(`${API_URL}/${id}`, {
+      method: 'DELETE'
+    })
     setTasks(tasks.filter((task) => task.id !== id));
   };
-  const toggleReminder = (id:number) => {
+
+  // const toggleReminder = (id:number) => {
+  //   setTasks(tasks.map((task) => 
+  //     (task.id === id) ? {...task, reminder: !task.reminder} : task
+  //   ))
+  // };
+  const toggleReminder = async (id:number) => {
     // console.log('App :: toggleReminder');`
+    const taskToToggle = await fetchATask(id);
+    const updTask = {...taskToToggle, reminder: !taskToToggle.reminder};
+    const res = await fetch(`${API_URL}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    });
+    const data = await res.json();
     setTasks(tasks.map((task) => 
-      (task.id === id) ? {...task, reminder: !task.reminder} : task
-    ))
+      (task.id === id) ? data : task)
+    )
   };
 
   return (
